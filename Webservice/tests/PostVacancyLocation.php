988d483c9e702778404ad467e81de4af24559fd8
Renan Fernandes <leandrorenanf@gmail.com>
2015-11-11 16:37:28 -0300

    [#106165930] Implementada função de pagamento utilizando créditos

---
+++ Webservice/tests/PostVacancyLocation.php
@@ -0,0 +1,71 @@
+<?php
+
+class PostVacancyLocation extends PHPUnit_Framework_TestCase {	
+	
+	public function testPostVacancyLocationInvalidSaldo(){
+		
+		/**
+		 * no banco localmente existe um user com id 1 e um vehicle com id 1
+		 * o saldo é menor que 120
+		 * un_price = 1
+		 */
+		 	
+		$postdata = http_build_query(
+		    array(
+		        'json' => "{\"IdUser\":\"1\",
+				 \"Plate\":\"kga9998\",
+				  \"Time\":\"120\"}"
+		    )
+		);
+		
+		$opts = array('http' =>
+		    array(
+		        'method'  => 'POST',
+		        'header'  => 'Content-type: application/x-www-form-urlencoded',
+		        'content' => $postdata
+		    )
+		);
+		
+		$context  = stream_context_create($opts);		
+		$response = file_get_contents('http://localhost/Webservice/post_vacancy_location_user', false, $context);		
+		$json = json_decode($response, true);
+		
+		$this->assertArrayHasKey('Error', $json);		
+		$this->assertEquals('Saldo insuficiente!', $json['Error']);
+		
+	}
+	
+	public function testPostVacanyLocationSucess(){
+		
+		/**
+		 * no banco localmente existe um user com id 1 e um vehicle com id 1
+		 * o saldo é menor que 120
+		 * un_price = 1
+		 */
+		 	
+		$postdata = http_build_query(
+		    array(
+		        'json' => "{\"IdUser\":\"1\",
+				 \"Plate\":\"kga1118\",
+				  \"Time\":\"10\"}"
+		    )
+		);
+		
+		$opts = array('http' =>
+		    array(
+		        'method'  => 'POST',
+		        'header'  => 'Content-type: application/x-www-form-urlencoded',
+		        'content' => $postdata
+		    )
+		);
+		
+		$context  = stream_context_create($opts);		
+		$response = file_get_contents('http://localhost/Webservice/post_vacancy_location_user', false, $context);		
+		$json = json_decode($response, true);
+		
+		$this->assertArrayHasKey('Sucess', $json);
+		echo $json['Sucess'];
+	}
+}
+
+?>
